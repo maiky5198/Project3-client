5f88143a83ac966304424dfd8c6d1357f7a2af52
“BishrJones” <bishr6jones@gmail.com>
2022-04-04 11:13:29 -0700

    api change

--- src/components/adventures/MyAdventures.js
+++ src/components/adventures/MyAdventures.js
@@ -18,8 +18,8 @@ const MyAdventures = (props) => {
     useEffect(() => {
         getMyAdventures()
             .then(res => {
-                console.log('res.data.adventures', res.data.adventures)
-                setMyAdventures(res.data.adventures)
+                console.log('res.data.adventures', res.data.myAdventures)
+                setMyAdventures(res.data.myAdventures)
             })
             
     }, [])
@@ -36,11 +36,11 @@ const MyAdventures = (props) => {
         adventureCards = myAdventures.map(adventure => (
             // one method of styling, usually reserved for a single style
             // we can use inline, just like in html
-            <Card key={adventure.id} style={{ width: '30%' }} className="m-2">
+            <Card key={adventure._id} style={{ width: '30%' }} className="m-2">
                 <Card.Header>{adventure.name}</Card.Header>
                 <Card.Body>
                     <Card.Text>
-                        <Link to={`/adventures/${adventure.id}`}>View {adventure.type}</Link>
+                        <Link to={`/adventures/${adventure._id}`}>View {adventure.type}</Link>
                     </Card.Text>
                 </Card.Body>
             </Card>
